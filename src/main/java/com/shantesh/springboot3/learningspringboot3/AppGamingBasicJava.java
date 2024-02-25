2d2f2e058ca91cbcc756204b04fd59379bcf0618
Shantesh <shanthu1804@gmail.com>
2024-02-25 19:23:19 +0530

    Pacman game impl is also added without changing the runner class, hence this is LOOSELY coupled

--- src/main/java/com/shantesh/springboot3/learningspringboot3/AppGamingBasicJava.java
+++ src/main/java/com/shantesh/springboot3/learningspringboot3/AppGamingBasicJava.java
@@ -2,13 +2,15 @@ package com.shantesh.springboot3.learningspringboot3;
 
 import com.shantesh.springboot3.learningspringboot3.game.GameRunner;
 import com.shantesh.springboot3.learningspringboot3.game.MarioGame;
+import com.shantesh.springboot3.learningspringboot3.game.PacmanGame;
 import com.shantesh.springboot3.learningspringboot3.game.SuperContraGame;
 
 public class AppGamingBasicJava {
 	
 	public static void main(String[] args) {
-		var game = new MarioGame();
+//		var game = new MarioGame();
 //		var game = new SuperContraGame();
+		var game = new PacmanGame();
 		var gameRunner = new GameRunner(game);
 		gameRunner.run();
 		
---
+++ src/main/java/com/shantesh/springboot3/learningspringboot3/game/PacmanGame.java
@@ -0,0 +1,18 @@
+package com.shantesh.springboot3.learningspringboot3.game;
+
+public class PacmanGame implements GamesInterface{
+	public void up() {
+		System.out.println("pac pac");
+	}
+	public void down() {
+		System.out.println("eat pac pac");
+	}
+	public void left() {
+		System.out.println("dance pac pac");
+	}
+	public void right() {
+		System.out.println("hip hop pac pac");
+	}
+
+
+}
