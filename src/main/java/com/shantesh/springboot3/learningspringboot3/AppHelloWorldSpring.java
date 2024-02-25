0238fa9e3affec4de179334c06354d5de2902170
Shantesh <shanthu1804@gmail.com>
2024-02-25 19:43:23 +0530

    Spring Context is launched by creating the Configuration class WITHOUT Bean creation in the configuration class to tell spring to manage the Bean. Ex: 19:40:09.745 [main] DEBUG org.springframework.beans.factory.support.DefaultListableBeanFactory - Creating shared instance of singleton bean 'helloWorldConfiguration'

---
+++ src/main/java/com/shantesh/springboot3/learningspringboot3/AppHelloWorldSpring.java
@@ -0,0 +1,18 @@
+package com.shantesh.springboot3.learningspringboot3;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.shantesh.springboot3.learningspringboot3.game.GameRunner;
+import com.shantesh.springboot3.learningspringboot3.game.MarioGame;
+import com.shantesh.springboot3.learningspringboot3.game.PacmanGame;
+import com.shantesh.springboot3.learningspringboot3.game.SuperContraGame;
+
+public class AppHelloWorldSpring {
+	
+	public static void main(String[] args) {
+//		Launch a Spring Context
+		var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+//		Configure the things that we want Spring to manage --> HelloWorldConfiguration.java
+	}
+
+}
---
+++ src/main/java/com/shantesh/springboot3/learningspringboot3/HelloWorldConfiguration.java
@@ -0,0 +1,9 @@
+package com.shantesh.springboot3.learningspringboot3;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloWorldConfiguration {
+
+}
