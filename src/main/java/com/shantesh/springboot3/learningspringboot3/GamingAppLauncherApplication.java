ea1ae76bef2088efe37a3175fefc7101b93129ee
Shantesh <shanthu1804@gmail.com>
2024-03-01 01:03:29 +0530

    further refactoring like removal of unnecessary class and some renaming the existing class to some meaningful thing

--- src/main/java/com/shantesh/springboot3/learningspringboot3/GamingAppLauncherApplication.java
+++ src/main/java/com/shantesh/springboot3/learningspringboot3/GamingAppLauncherApplication.java
@@ -1,21 +1,20 @@
 package com.shantesh.springboot3.learningspringboot3;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.shantesh.springboot3.learningspringboot3.game.GameRunner;
 import com.shantesh.springboot3.learningspringboot3.game.GamesInterface;
-import com.shantesh.springboot3.learningspringboot3.game.PacmanGame;
 
 @Configuration
 @ComponentScan("com.shantesh.springboot3.learningspringboot3")
-public class AppGamingBasicJava {
+public class GamingAppLauncherApplication {
 	
+
 	public static void main(String[] args) {
 
-		try (var context = new AnnotationConfigApplicationContext(AppGamingBasicJava.class)) {
+		try (var context = new AnnotationConfigApplicationContext(GamingAppLauncherApplication.class)) {
 			context.getBean(GamesInterface.class).up();
 			context.getBean(GameRunner.class).run();
 		}
