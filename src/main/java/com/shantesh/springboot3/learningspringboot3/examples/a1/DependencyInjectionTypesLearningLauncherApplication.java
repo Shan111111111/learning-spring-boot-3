c1c46f11c9140fb2dcad306edf92fcaf3f044097
Shantesh <shanthu1804@gmail.com>
2024-03-02 00:21:21 +0530

    without dependency Injection but just by declaring fields Ex: Using ---> null and null

--- src/main/java/com/shantesh/springboot3/learningspringboot3/examples/a1/DependencyInjectionTypesLearningLauncherApplication.java
+++ src/main/java/com/shantesh/springboot3/learningspringboot3/examples/a1/DependencyInjectionTypesLearningLauncherApplication.java
@@ -1,37 +1,48 @@
 package com.shantesh.springboot3.learningspringboot3.examples.a1;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Component
-class YourBusinessClass{
-	
+class YourBusinessClass {
+	Dependency1 dependency1;
+	Dependency2 dependency2;
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Using ---> " + dependency1 + " and " + dependency2;
+	}
+
 }
+
 @Component
-class Dependency1{
-	
+class Dependency1 {
+
 }
+
 @Component
-class Dependency2{
-	
+class Dependency2 {
+
 }
 
 @Configuration
 @ComponentScan
 public class DependencyInjectionTypesLearningLauncherApplication {
-	
 
 	public static void main(String[] args) {
 
-		try (var context = new AnnotationConfigApplicationContext(DependencyInjectionTypesLearningLauncherApplication.class)) {
+		try (var context = new AnnotationConfigApplicationContext(
+				DependencyInjectionTypesLearningLauncherApplication.class)) {
+			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 
-
+			System.out.println(context.getBean(YourBusinessClass.class));
 		}
 
 	}
 
 }
-
-
