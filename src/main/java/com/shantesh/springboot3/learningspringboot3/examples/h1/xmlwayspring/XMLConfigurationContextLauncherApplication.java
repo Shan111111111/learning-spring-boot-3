115b2a234ae969296facaa76f916b994a1c6bfba
Shantesh <shanthu1804@gmail.com>
2024-03-02 18:51:44 +0530

    ================ Old way of XML way of spring context generation and bean creation and scanning and autowiring, getting started ==========

---
+++ src/main/java/com/shantesh/springboot3/learningspringboot3/examples/h1/xmlwayspring/XMLConfigurationContextLauncherApplication.java
@@ -0,0 +1,20 @@
+package com.shantesh.springboot3.learningspringboot3.examples.h1.xmlwayspring;
+
+import java.util.Arrays;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class XMLConfigurationContextLauncherApplication {
+	
+	public static void main(String[] args) {
+
+		try (var context = new ClassPathXmlApplicationContext("contextConfiguration.xml")) {
+			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+
+		}
+
+	}
+
+}
+
+
