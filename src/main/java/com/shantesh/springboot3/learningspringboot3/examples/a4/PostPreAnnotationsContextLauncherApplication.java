9c4daffd4cad3ebb0943b154e8f76b2f6d98337b
Shantesh <shanthu1804@gmail.com>
2024-03-02 17:53:01 +0530

    demostration  @PostConstruct--> calling selected methods after bean creation/instantiation/bean's constructor's call AND @PreDestroy--> calling few/some methods before destroying of bean Ex: 17:49:59.390 [main] DEBUG org.springframework.beans.factory.support.DefaultListableBeanFactory - Creating shared instance of singleton bean 'postPreAnnotationsContextLauncherApplication' 17:49:59.447 [main] DEBUG org.springframework.beans.factory.support.DefaultListableBeanFactory - Creating shared instance of singleton bean 'someClass' 17:49:59.521 [main] DEBUG org.springframework.beans.factory.support.DefaultListableBeanFactory - Creating shared instance of singleton bean 'someDependency' 17:49:59.536 [main] DEBUG org.springframework.beans.factory.support.DefaultListableBeanFactory - Autowiring by type from bean name 'someClass' via constructor to bean named 'someDependency' ..........All dependencies are ready....... some initialization logic ======>>>>>>>> some logic using/in SomeDependency some initialization logic >>>>>>>> some logic using/in SomeDependency 17:49:59.797 [main] DEBUG org.springframework.context.annotation.AnnotationConfigApplicationContext - Closing org.springframework.context.annotation.AnnotationConfigApplicationContext@7c30a502, started on Sat Mar 02 17:49:58 IST 2024 some clean Up logic <----->

--- src/main/java/com/shantesh/springboot3/learningspringboot3/examples/a4/PostPreAnnotationsContextLauncherApplication.java
+++ src/main/java/com/shantesh/springboot3/learningspringboot3/examples/a4/PostPreAnnotationsContextLauncherApplication.java
@@ -3,7 +3,47 @@ package com.shantesh.springboot3.learningspringboot3.examples.a4;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
+@Component
+class SomeClass {
+	SomeDependency someDependency;
+
+	public SomeClass(SomeDependency someDependency) {
+		super();
+		this.someDependency = someDependency;
+		System.out.println("..........All dependencies are ready.......");
+	}
+
+	@PostConstruct
+	public void initialize() {
+		System.out.println("some initialization logic ======>>>>>>>>");
+		someDependency.getReady();
+	}
+
+	@PostConstruct
+	public void initialize2() {
+		System.out.println("some initialization logic >>>>>>>>");
+		someDependency.getReady();
+	}
+
+	@PreDestroy
+	public void cleanUp() {
+		System.out.println("some clean Up logic <----->");
+	}
+}
+
+@Component
+class SomeDependency {
+
+	public void getReady() {
+		System.out.println("some logic using/in SomeDependency");
+	}
+
+}
 
 @Configuration
 @ComponentScan
