ec8bae0e40107bc56c87159c43fbd917cf74b25e
Shantesh <shanthu1804@gmail.com>
2024-03-02 15:28:15 +0530

    bean/object scope changed to prototype

--- src/main/java/com/shantesh/springboot3/learningspringboot3/examples/a3/BeanScopesLauncherApplication.java
+++ src/main/java/com/shantesh/springboot3/learningspringboot3/examples/a3/BeanScopesLauncherApplication.java
@@ -2,9 +2,11 @@ package com.shantesh.springboot3.learningspringboot3.examples.a3;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,6 +14,7 @@ class NormalClass {
 
 }
 
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 @Component
 class PrototypeClass {
 
