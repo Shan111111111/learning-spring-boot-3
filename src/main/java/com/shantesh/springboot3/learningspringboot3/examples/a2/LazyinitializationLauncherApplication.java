419c261cb1b00797636101319bc78751b77bcc7a
Shantesh <shanthu1804@gmail.com>
2024-03-02 15:06:16 +0530

    Lazy initialization/constructor injection/autowiring is NOT happening as @Lazy is used and field not initialized Ex: org.springframework.context.annotation.internalConfigurationAnnotationProcessor org.springframework.context.annotation.internalAutowiredAnnotationProcessor org.springframework.context.annotation.internalCommonAnnotationProcessor org.springframework.context.event.internalEventListenerProcessor org.springframework.context.event.internalEventListenerFactory lazyinitializationLauncherApplication classA classB

--- src/main/java/com/shantesh/springboot3/learningspringboot3/examples/a2/LazyinitializationLauncherApplication.java
+++ src/main/java/com/shantesh/springboot3/learningspringboot3/examples/a2/LazyinitializationLauncherApplication.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 @Component
 class ClassA{
@@ -12,6 +13,7 @@ class ClassA{
 	
 }
 @Component
+@Lazy
 class ClassB{
 	ClassA classA;
 	
