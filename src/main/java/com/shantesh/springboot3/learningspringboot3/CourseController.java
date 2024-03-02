3ce2fc881443677d4f08e437da0eb7af20160b0b
Shantesh <shanthu1804@gmail.com>
2024-03-03 00:39:36 +0530

    Simple cotroller with exposing list of courses are written

--- src/main/java/com/shantesh/springboot3/learningspringboot3/CourseController.java
+++ src/main/java/com/shantesh/springboot3/learningspringboot3/CourseController.java
@@ -1,5 +1,8 @@
 package com.shantesh.springboot3.learningspringboot3;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +12,10 @@ public class CourseController {
 	@GetMapping("/")
 	public String returnResponse() {
 		return "this is Course from the rest controller";
+	}
+	
+	@GetMapping("/courses")
+	public List<Course> retrieveAllCourses() {
+		return Arrays.asList(new Course(3, "sharima", "the man of war"), new Course(3, "Daniel steel", "the neighbours"));
 	}
 }
