f11c90ac5d32d223a697ffaa098db266f2975949
Shantesh <shanthu1804@gmail.com>
2024-03-02 13:54:47 +0530

    Interface, its implementations and Interface utilizing business class is added with spring launcher

--- src/main/java/com/shantesh/springboot3/learningspringboot3/examples/exercise/ExerciseLauncherApplication.java
+++ src/main/java/com/shantesh/springboot3/learningspringboot3/examples/exercise/ExerciseLauncherApplication.java
@@ -1,10 +1,40 @@
 package com.shantesh.springboot3.learningspringboot3.examples.exercise;
 
 import java.util.Arrays;
+import java.util.OptionalInt;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+interface DataService{
+	int[] retrieveData();
+}
+
+class MongoDataService implements DataService{
+	public int[] retrieveData() {
+		System.out.println("in MongoDataService...");
+		return new int[] {11, 22, 33, 44};
+	}
+}
+
+class MYSQLDataService implements DataService{
+	public int[] retrieveData() {
+		System.out.println("in MYSQLDataService...");
+		return new int[] {111, 222, 333, 444};
+	}
+}
+
+class BusinessCalculationService {
+
+	DataService dataService;
+	
+	public OptionalInt findMax() {
+		return Arrays.stream(dataService.retrieveData()).max();
+	}
+	
+}
+
 
 @Configuration
 @ComponentScan
@@ -14,6 +44,7 @@ public class ExerciseLauncherApplication {
 
 		try (var context = new AnnotationConfigApplicationContext(ExerciseLauncherApplication.class)) {
 			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+
 
 		}
 
